f41ad7bd034fa02effc3d186b581471d1a373ba5
Azeem <azeem.ahmed@cloudasset.com>
2018-08-02 05:42:49 +0500

    Initial Commit

---
+++ The Holy Touch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Holy Touch
+//
+//  Created by HansaMAC on 8/2/18.
+//  Copyright © 2018 Hajj Hackathon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Holy TouchTests/The_Holy_TouchTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Holy_TouchTests.swift
+//  The Holy TouchTests
+//
+//  Created by HansaMAC on 8/2/18.
+//  Copyright © 2018 Hajj Hackathon. All rights reserved.
+//
+
+import XCTest
+@testable import The_Holy_Touch
+
+class The_Holy_TouchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
